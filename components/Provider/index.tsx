1b71a9a9c1f01ccfb1d116c211e2c4d04b3cda5f
kumailnaqvi354 <komailnaqvi616@gmail.com>
2025-04-21 18:32:46 +0100

    update list-property page for web3 provider

--- components/Provider/index.tsx
+++ components/Provider/index.tsx
@@ -1,9 +1,9 @@
 'use client'
+import { config } from '@/lib/web3-config';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ConnectKitProvider } from 'connectkit';
 import React from 'react'
 import { WagmiProvider } from 'wagmi';
-import { config } from '../../lib/web3-config';
 
 
 function Web3Provider({ children }: { children: React.ReactNode }) {
